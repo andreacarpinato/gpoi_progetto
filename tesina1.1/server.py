35ea14fcf515cde665f23a8194ea7115ff83ec9c
andrewcarpi <46714914+andrewcarpi@users.noreply.github.com>
2020-05-30 19:54:57 +0200

    Add files via upload

--- tesina1.1/server.py
+++ tesina1.1/server.py
@@ -1,26 +1,34 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 import sqlite3
+import webbrowser
 
 app = Flask(__name__)
 Bootstrap(app)
+db='data.db'
 
 @app.route("/",methods=['GET', 'POST'])
 def home():
-    return render_template("index.html")
-'''
+  return render_template("index.html")
+ 
 @app.route("/allbooks",methods=['GET', 'POST'])
 def visualizzaTutto():
   #  name=#read from db sqlite
-
-    return render_template("visualizza.html",nomeLibro=name,autore=autore,annoPubblicazione=anno)
+    return "<p>ciao</p>"
+   # return render_template("visualizza.html",nomeLibro=name,autore=autore,annoPubblicazione=anno)
 
 @app.route('/addbook',methods=['GET','POST'])
-def aggiungi(name,author,)
+def aggiungi():
+  return "<p>va libro</p>"  #render_template("agglibro.html")
 
 '''
+@app.route('/research',methods=['GET','POST'])
+def ricerca():
+
+  return 
+'''
 if __name__ == "__main__":
-    app.run()
+  app.run()
 
 
 
